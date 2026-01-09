5adce87d5d934b28936f32e90b25c1d69b4ab80f
zhn2605 <zzihan0805@gmail.com>
2026-01-08 20:13:07 -0500

    export static

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
